7ad3f9bf72855f11f645b1367d30f22be3ca2587
dongledog <dongledog@gmail.com>
2020-03-26 20:16:36 +0900

    최소한의 Crawler 기능 구현

---
+++ TradingBot/TradingBot.Crawler/InvestingChartData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingBot.Crawler
+{
+    public class InvestingChartData
+    {
+        /// <summary>
+        /// unixtimestamp 형식 차트 시간
+        /// </summary>
+        public List<int> t { get; set; }
+
+        /// <summary>
+        /// close
+        /// </summary>
+        public List<double> c { get; set; }
+
+        /// <summary>
+        /// open
+        /// </summary>
+        public List<double> o { get; set; }
+
+        /// <summary>
+        /// high
+        /// </summary>
+        public List<double> h { get; set; }
+
+        /// <summary>
+        /// low
+        /// </summary>
+        public List<double> l { get; set; }
+
+        /// <summary>
+        /// status? ok여야 정상
+        /// </summary>
+        public string s { get; set; }
+    }
+}
---
+++ TradingBot/TradingBot.Crawler/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TradingBot.Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () => await AsyncMain()).Wait();
+        }
+
+        public static async Task AsyncMain()
+        {
+            int lastTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            int gap = 1296060;
+            int symbol = 8849;
+
+            using (var httpClient = new HttpClient())
+            {
+                while (true)
+                {
+                    httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36");
+
+                    var response = await httpClient.GetAsync(
+                        $"https://tvc4.forexpros.com/f4ea20b925889f8ab599a646581d040a/1585218404/18/18/88/history?symbol={symbol}&resolution=1&from={lastTime - gap}&to={lastTime}");
+
+
+                    var chartData =
+                        JsonConvert.DeserializeObject<InvestingChartData>(await response.Content.ReadAsStringAsync());
+
+                    if (chartData.s != "ok")
+                    {
+                        // 정상 응답이 아니면 멈춘다
+                        break;
+                    }
+
+                    var firstTime = chartData.t.First();
+                    Console.WriteLine(firstTime);
+                    lastTime = firstTime - 1;
+                }
+            }
+        }
+    }
+}
